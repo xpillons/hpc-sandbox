ab9e4317efaac5cc26294e76f68c3752ea019c1e
Xavier Pillons <xpillons@microsoft.com>
2019-02-12 14:42:23 +0100

    fix

--- create_nfs.sh
+++ create_nfs.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
+resource_group=$1
+vnetname=$2
+
 rsaPublicKey=$(cat ~/.ssh/id_rsa.pub)
 
 parameters=$(cat << EOF
 {
-    "adminUser": {
-        "value": ""
-    },
     "vnetName": {
         "value": "$vnetname"
     },
@@ -14,7 +16,7 @@ parameters=$(cat << EOF
 }
 EOF
 )
-resource_group=""
+
 
 az group deployment create \
     --resource-group "$resource_group" \
