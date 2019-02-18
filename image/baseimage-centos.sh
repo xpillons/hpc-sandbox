02be365963c2d598486e5ac4c5d5b069e0ed9d53
Xavier Pillons <xpillons@microsoft.com>
2019-02-18 16:13:24 +0100

    added LustrePack.repo

--- image/baseimage-centos.sh
+++ image/baseimage-centos.sh
@@ -133,13 +133,23 @@ install_lustre()
     case "$vm_size" in
         *_h16*) 
             build_lustreRPM
+            kmod-lustre-client=""
             ;;
         *)
-            yum install -y kmod-lustre-client
+            kmod-lustre-client="kmod-lustre-client"
             ;;
     esac
 
-    yum install -y https://downloads.whamcloud.com/public/lustre/lustre-2.10.6/el7/client/RPMS/x86_64/lustre-client-2.10.6-1.el7.x86_64.rpm
+
+cat << EOF >/etc/yum.repos.d/LustrePack.repo
+[lustreclient]
+name=lustreclient
+baseurl=https://downloads.whamcloud.com/public/lustre/latest-2.10-release/el7.6.1810/client/
+enabled=1
+gpgcheck=0
+EOF
+    yum install -y lustre-client kmod-lustre-client
+    #yum install -y kmod-lustre-client https://downloads.whamcloud.com/public/lustre/lustre-2.10.6/el7/client/RPMS/x86_64/lustre-client-2.10.6-1.el7.x86_64.rpm
     mkdir /mnt/lustre
 }
 
