2e6aeda52d98695333fb6caed40fffd4b2e89bdf
Xavier Pillons <xpillons@microsoft.com>
2019-02-25 11:55:24 +0100

    fix /etc/rdma/dat.conf update

--- vmss/set_eth1_for_rdma.sh
+++ vmss/set_eth1_for_rdma.sh
@@ -4,6 +4,6 @@
 
 IP_ADDRESS=$(sed '/rdmaIPv4Address=/!d;s/.*rdmaIPv4Address="\([0-9.]*\)".*/\1/' /var/lib/waagent/SharedConfig.xml)
 ifconfig eth1 ${IP_ADDRESS}/16
-sed -i "s/ofa-v2-ib0 u2.0 nonthreadsafe default libdaplofa.so.2 dapl.2.0 \".*$/ofa-v2-ib0 u2.0 nonthreadsafe default libdaplofa.so.2 dapl.2.0 \"$IP_ADDRESS\" \"\"/g" /etc/rdma/dat.conf
+sed -i "s/ofa-v2-ib0 u2.0 nonthreadsafe default libdaplofa.so.2 dapl.2.0 \".*$/ofa-v2-ib0 u2.0 nonthreadsafe default libdaplofa.so.2 dapl.2.0 \"$IP_ADDRESS 0\" \"\"/g" /etc/rdma/dat.conf
 
 ifconfig
